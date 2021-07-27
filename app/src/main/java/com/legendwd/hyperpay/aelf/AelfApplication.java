112cc616f533d4f7f659c7b5a076a97b41780502
aelf-hz <mason.huang@aelf.io>
2021-07-27 17:34:28 +0800

    refactor: 💡 remove qbsdk

--- app/src/main/java/com/legendwd/hyperpay/aelf/AelfApplication.java
+++ app/src/main/java/com/legendwd/hyperpay/aelf/AelfApplication.java
@@ -30,7 +30,7 @@ import com.legendwd.hyperpay.lib.Util;
 import com.legendwd.hyperpay.router.IMainService;
 import com.legendwd.hyperpay.router.Router;
 import com.tencent.bugly.crashreport.CrashReport;
-import com.tencent.smtt.sdk.QbSdk;
+// import com.tencent.smtt.sdk.QbSdk;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.message.IUmengRegisterCallback;
 import com.umeng.message.PushAgent;
@@ -147,22 +147,22 @@ public class AelfApplication extends MultiDexApplication {
                 SPWrapper.setContext(sContext);
 
 
-                //搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
+                // //搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
 
 
-                QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
+                // QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
 
-                    @Override
-                    public void onViewInitFinished(boolean arg0) {
-                        //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
-                    }
+                //     @Override
+                //     public void onViewInitFinished(boolean arg0) {
+                //         //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
+                //     }
 
-                    @Override
-                    public void onCoreInitFinished() {
-                    }
-                };
-                //x5内核初始化接口
-                QbSdk.initX5Environment(getApplicationContext(), cb);
+                //     @Override
+                //     public void onCoreInitFinished() {
+                //     }
+                // };
+                // //x5内核初始化接口
+                // QbSdk.initX5Environment(getApplicationContext(), cb);
             }
         });
         initGreenDao();
